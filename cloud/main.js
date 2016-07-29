76812c561fab49d6b2e3b5bd1b27ee3937caabe8
Emilio Arroyo-Fang <earroyof@fb.com>
2016-07-29 15:14:38 -0700

    added push code

--- cloud/main.js
+++ cloud/main.js
@@ -3,6 +3,103 @@ var kue = require('kue');
 // create our job queue
 var jobs = kue.createQueue({ redis: process.env.REDIS_URL });
 
+
+
+Parse.Cloud.define('alertAllWithPushOn', function(request, response) {
+    var query = new Parse.Query(Parse.User); // Query for all users
+    // For each user in database
+    query.each(function(user) {
+        var hasPushOn = user.get('push');
+        // Only execute if user has push setting on
+        if (hasPushOn === true) {
+            var hasExpired = [];
+            var willExpire = [];
+            // Current Date
+            var now = new Date();
+            // Create Date limit
+            var offset = user.get('warning_offset');
+            var expireDate = new Date(now);
+            expireDate.setDate(expireDate.getDate() + offset);
+            var foodQueryHasExpired = new Parse.Query("Food");
+            foodQueryHasExpired.equalTo("owner", user);
+            foodQueryHasExpired.lessThan("expiration_date", now);
+
+            var foodQueryWillExpire = new Parse.Query("Food");
+            foodQueryWillExpire.equalTo("owner", user);
+            foodQueryWillExpire.greaterThan("expiration_date", now);
+            foodQueryWillExpire.lessThan("expiration_date", expireDate);
+
+            foodQueryHasExpired.find({
+                success: function(results) {
+                             for (var i = 0; i < results.length; i++) {
+                                 var object = results[i].get("product_name");
+                                 hasExpired.push(object);
+                             }
+                         },
+                error: function(error) {
+                           // Do nothing
+                       }
+            });
+
+            foodQueryWillExpire.find({
+                success: function(results) {
+                             for (var i = 0; i < results.length; i++) {
+                                 var object = results[i].get("product_name");
+                                 willExpire.push(object);
+                             }
+                         },
+                error: function(error) {
+                           // Do nothing
+                       }
+            });
+
+            var message = "";
+            if (hasExpired.length > 0) {
+                message += "Expired: ";
+                for (var i = 0; i < hasExpired.length - 1; i++) {
+                    message += hasExpired[i] + ", ";
+                }
+                if (hasExpired.length > 1) {
+                    message += "and ";
+                }
+                message += hasExpired[hasExpired.length - 1] + ". ";
+            }
+
+            if (willExpire.length > 0) {
+                message += "Expiring Soon: ";
+                for (var i = 0; i < willExpire.length - 1; i++) {
+                    message += willExpire[i] + ", ";
+                }
+                if (willExpire.length > 1) {
+                    message += "and ";
+                }
+
+                message += willExpire[willExpire.length - 1] + ". ";
+            }
+
+            if (message !== "") {
+                var pushQuery = new Parse.Query(Parse.Installation);
+                pushQuery.equalTo("deviceType", "android");
+                pushQuery.equalTo("user", user);
+                Parse.Push.send({
+                    where: pushQuery,
+                    data: {
+                        alert: message
+                    }
+                }, { success: function() {
+                    console.log("PUSH OK");
+                }, error: function(error) {
+                    console.log("PUSH ERROR: " + error.message);
+                }, useMasterKey: true});
+
+                response.success("success");
+            }
+        };
+    });
+});
+
+
+
 // Push with promises
 Parse.Cloud.define('androidPushTest', function(request, response) {
     // request has 2 parameters: params passed by the client and the authorized user
